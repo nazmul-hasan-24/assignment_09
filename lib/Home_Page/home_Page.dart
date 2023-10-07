a3c393c16fdd68411289e97218d7c4bed00808bb
Nazmul Hasan <130824854+nazmul-hasan-24@users.noreply.github.com>
2023-10-07 23:35:11 +0600

    Update home_Page.dart

--- lib/Home_Page/home_Page.dart
+++ lib/Home_Page/home_Page.dart
@@ -70,7 +70,7 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text("Total Amount: "), 
-              // Text("${}")
+              Text("250\$")
               ],
           ),
           SizedBox(
